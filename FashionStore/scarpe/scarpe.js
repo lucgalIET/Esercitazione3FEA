fdc5fba60a8e2b1ed61f12231637b178ca6d3a1b
EnderModZe <manuel.arquati@gmail.com>
2023-07-14 11:48:02 +0200

    final commit

--- FashionStore/scarpe/scarpe.js
+++ FashionStore/scarpe/scarpe.js
@@ -10,3 +10,10 @@ document.getElementById("button2").addEventListener('click', function(event) {
     toAdd.removeChild(document.getElementById("button2"));    console.log(toAdd);
     document.getElementById("carrello").appendChild(toAdd);
 })
+
+document.getElementById("button3").addEventListener('click', function(event) {
+    let toAdd = document.getElementById("3");
+    toAdd.removeChild(document.getElementById("button3"));    console.log(toAdd);
+    document.getElementById("carrello").appendChild(toAdd);
+})
+
