22663e84f47385637cdd9a796915437f228a58d7
EnderModZe <manuel.arquati@gmail.com>
2023-07-14 13:26:13 +0200

    Home e fix

--- FashionStore/scarpe/scarpe.js
+++ FashionStore/scarpe/scarpe.js
@@ -5,6 +5,13 @@ document.getElementById("button1").addEventListener('click', function(event) {
     document.getElementById("carrello").appendChild(toAdd);
 })
 
+/*document.getElementById("button1").addEventListener('click', function(event) {
+    let toAdd = document.getElementById("1");
+    toAdd.removeChild(document.getElementById("button1"));
+    console.log(toAdd);
+    document.getElementById("carrello").appendChild(toAdd);
+})*/
+
 document.getElementById("button2").addEventListener('click', function(event) {
     let toAdd = document.getElementById("2");
     toAdd.removeChild(document.getElementById("button2"));    console.log(toAdd);
@@ -15,5 +22,9 @@ document.getElementById("button3").addEventListener('click', function(event) {
     let toAdd = document.getElementById("3");
     toAdd.removeChild(document.getElementById("button3"));    console.log(toAdd);
     document.getElementById("carrello").appendChild(toAdd);
+})
+
+document.getElementById("removeAll").addEventListener("click", function(event) {
+    location.reload();
 })
 
