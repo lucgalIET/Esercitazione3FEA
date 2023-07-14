906de7e6729d4c242b48e79bcad5f6037ea3de50
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-07-14 13:35:59 +0200

    aggiunta bottone "elimina" dal carrello

--- FashionStore/pantaloni/pantaloni.js
+++ FashionStore/pantaloni/pantaloni.js
@@ -2,10 +2,13 @@ function addCartItem(){
     console.log("sono nella funzione");
    let items =  document.getElementById("carrello");
    let prezzo = document.getElementById("prezzoId");
-   let prezzoValore = prezzo.textContent;
+    let prezzoValore = prezzo.textContent;
     let newElem= document.createElement("a");
+    let newBtn = document.createElement("button");
+    newBtn.className="fa fa-close"
     newElem.textContent="pantalone "+prezzoValore;
    //newElem.innerHTML="pantalone aggiunto";
+   newElem.appendChild(newBtn);
    items.appendChild(newElem);
 }
 
